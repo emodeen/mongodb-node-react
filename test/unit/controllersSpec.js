34ae92bd1e9b49e73f3f4414864fcf704d7f3a28
Eric Modeen <emodeen@gmail.com>
2015-01-23 21:41:25 -0500

    Removed unused call to expectGET in HipaaReqCtrl

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -20,9 +20,6 @@ describe('PhoneCat controllers', function() {
 
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/phones.json').
-          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
-
       scope = $rootScope.$new();
       ctrl = $controller('HipaaReqCtrl', {$scope: scope});
     }));
