f859024c94488d61ca82dd9f2ef7ddf7306eb3f9
emodeen <emodeen@gmail.com>
2016-06-14 18:01:04 -0400

    Added react-components.js

--- app/js/react-components.js
+++ app/js/react-components.js
@@ -1,14 +1,34 @@
-var data = [
-  {id: 1, author: "Clint Eastwood", text: "This is one comment"},
-  {id: 2, author: "Eric Modeen", text: "This is *another* comment"}
-];
+
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function() {
+    return {data: []};
+  },
+
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval); 	
+  },
+
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />        
+        <CommentList data={this.state.data} />        
         <CommentForm />
       </div>
     );
@@ -56,6 +76,6 @@ var CommentForm = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox data={data} />,	
+  <CommentBox url="/api/comments" pollInterval={2000} />,	
   document.getElementById('content')
 );      
