c786e8ca9628dc82a85a966f51aad93d37871d0a
emodeen <emodeen@gmail.com>
2016-06-17 07:46:44 -0400

    Changed react js file referenced in index.html

--- app/js/react-components.js
+++ app/js/react-components.js
@@ -76,6 +76,6 @@ var CommentForm = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox url="/api/comments" pollInterval={2000} />,	
+  <CommentBox url="/api/comments"/>,	
   document.getElementById('content')
 );      
